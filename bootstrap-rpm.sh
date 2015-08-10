76491a5b2c0312abfd86fde1534553ed3217abe0
Paul Massey <paul.massey@tenforce.com>
2015-08-10 13:15:38 +0200

    missed bootstrap file

---
+++ bootstrap-rpm.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+#################################################################
+# Digital Agenda Score Board (build system requirements)
+
+#################################################################
+# Sort out basic + docker installation for centos 7
+# yum -y install epel-release-7
+yum -y update
+
+yum -y groupinstall "Desktop" "Desktop Platform" "X Window System" "Fonts"
+yum -y groupinstall "Graphical Administration Tools"
+yum -y groupinstall "Internet Browser"
+yum -y groupinstall "General Purpose Desktop"
+yum -y groupinstall "Office Suite and Productivity"
+yum -y groupinstall "Graphics Creation Tools"
+
+# Check boot init (change to graphical)
+sed -i -e 's/:3:/:5:/g' /etc/inittab
+
+# Install decent editor
+yum --enablerepo rpmforge install dkms
+yum -y install kernel-devel-`uname -r` kernel-headers-`uname -r`
+yum -y install kernel-devel kernel-headers
+yum -y install dos2unix firefox emacs
+yum -y install emacs autoconf
+
+#################################################################
+# finishing and cleaning up.
+echo "****** done with bootstrap of Digital Agenda Test machine"
